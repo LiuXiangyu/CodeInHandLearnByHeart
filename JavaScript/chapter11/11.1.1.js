478219f529fc698cbd797d532ab8d6416c39fb07
liuxiangyu <liuxiangyu@live.com>
2015-04-16 20:27:26 +0800

    update

---
+++ JavaScript/chapter11/11.1.1.js
@@ -0,0 +1,8 @@
+var body = document.querySelector("body");
+var myDiv = document.querySelector("#myDiv");
+var selected = document.querySelector(".selected");
+var img = document.body.querySelector("img.button");
+
+var ems = document.getElementById("myDiv").querySelectorAll("em");
+var selecteds = document.querySelectorAll(".selected");
+var strongs = document.querySelectorAll("p strong");
--- javascript/chapter10/10.1.1.js
+++ javascript/chapter10/10.1.1.js
@@ -24,3 +24,35 @@ function convertToArray(nodes) {
 
     return array;
 }
+
+var returnNode = someNode.appendChild(newNode);
+alert(returnNode == newNode); // true
+alert(someNode.lastChild == newNode); // true
+
+var returnNode = someNode.appendChild(someNode.firstChild);
+alert(returnNode == someNode.firstChild); // false
+alert(returnNode == someNode.lastChild); // true
+
+returnNode = someNode.insertBefore(newNode, null);
+alert(newNode == someNode.lastChild); // true
+
+var returnNode = someNode.insertBefore(newNode, someNode.firstChild);
+alert(returnNode == newNode); // true
+alert(newNode == someNode.firstChild); // true;
+
+returnNode = someNode.insertBefore(newNode, someNode.lastChild);
+alert(newNode == someNode.childNodes[someNode.childNodes.length - 2]);
+
+var returnNode = someNode.replace(newNode, someNode.firstChild);
+
+returnNode = someNode.replaceChild(newNode, someNode.lastChild);
+
+var formerFirstChild = someNode.removeChild(someNode.firstChild);
+
+var formerLastChild = someNode.removeChild(someNode.lastChild);
+
+var deepList = myList.cloneNode(true);
+alert(deepList.childNodes.length); // 3
+
+var shallowList = myList.cloneNode(false);
+alert(shallowList.childNodes.length); // 0
