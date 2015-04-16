468e5868dd259bd6b8725f430ace3a9fbdfb45d8
liuxiangyu <liuxiangyu@live.com>
2015-04-16 13:38:02 +0800

    update

---
+++ javascript/chapter10/10.2.3/main.js
@@ -0,0 +1,30 @@
+//创建table
+var table = document.createElement("table");
+table.border = 1;
+table.width = "100%";
+
+//创建tbody
+var tbody = document.createElement("tbody");
+table.appendChild(tbody);
+
+//创建第一行
+var row1 = document.createElement("tr");
+tbody.appendChild(row1);
+var cell1_1 = document.createElement("td");
+cell1_1.appendChild(document.createTextNode("Cell 1, 1"));
+row1.appendChild(cell1_1);
+var cell2_1 = document.createElement("td");
+cell2_1.appendChild(document.createTextNode("Cell 2, 1"));
+row1.appendChild(cell2_1);
+
+//创建第二行
+var row2 = document.createElement("tr");
+tbody.appendChild(row2);
+var cell1_2 = document.createElement("td");
+cell1_2.appendChild(document.createTextNode("Cell 1, 2"));
+row2.appendChild(cell1_2);
+var cell2_2 = document.createElement("td");
+cell2_2.appendChild(document.createTextNode("Cell 2, 2"));
+row2.appendChild(cell2_2);
+
+document.body.appendChild(table);
